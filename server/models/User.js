5391cab0d66062fc2de4a94ad7bcbc8f1ba79cea
navneet567 <navneetsinghcreation@gmail.comn>
2026-07-30 14:24:59 +0530

    Fix Mongoose async pre-save hook next is not a function error

--- server/models/User.js
+++ server/models/User.js
@@ -36,13 +36,12 @@ const userSchema = new mongoose.Schema(
 );
 
 // Hash password before saving to the database
-userSchema.pre("save", async function (next) {
+userSchema.pre("save", async function () {
   if (!this.isModified("password") || !this.password) {
-    return next();
+    return;
   }
   const salt = await bcrypt.genSalt(10);
   this.password = await bcrypt.hash(this.password, salt);
-  next();
 });
 
 // Method to compare entered password with hashed password
